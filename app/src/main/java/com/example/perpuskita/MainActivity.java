a9cd8321aea8d074df921c57350e1834bd80754a
Yasya Rusyda <yasyars@Yasyas-MacBook-Pro.local>
2019-02-24 00:09:42 +0700

    Add home fragment

--- app/src/main/java/com/example/perpuskita/MainActivity.java
+++ app/src/main/java/com/example/perpuskita/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.perpuskita;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //deklarasi variabel
+    private TabLayout tabs;
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //connect to activity_main.xml (resource ui dan id)
         setContentView(R.layout.activity_main);
+
+        //inisialisasi sesuai id yg ada di activity_main yang tipenya sesuai deklarasi variabel
+        tabs = findViewById(R.id.tabs);
+        pager = findViewById(R.id.pager);
+
+        //inisialisasi viewpageradapter
+        ViewPagerAdapter myPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Add titles to each tab
+        myPagerAdapter.addFragment(new BorrowFragment(),"Pinjam ");
+        myPagerAdapter.addFragment(new BooksFragment(),"Cari");
+        myPagerAdapter.addFragment(new ProfileFragment(), "Profil");
+
+        //add adapter to pager
+        pager.setAdapter(myPagerAdapter);
+
+        //connect tab with pager
+        tabs.setupWithViewPager(pager);
+
+        //styling tab
+        tabs.setTabTextColors(getResources().getColor(R.color.colorAccent),
+                getResources().getColor(R.color.colorAccent));
+        tabs.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabs.setTabMode(TabLayout.MODE_FIXED);
+
+        int[] tabIcon = {
+                R.drawable.borrow_icon,
+                R.drawable.search_book,
+                R.drawable.profile_person
+        };
+
+        for (int i = 0; i<tabIcon.length;i++){
+            TabLayout.Tab tab = tabs.getTabAt(i);
+            if (tab != null) tab.setIcon(tabIcon[i]);
+        }
     }
 }
---
+++ app/src/main/java/com/example/perpuskita/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.perpuskita;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> mFragmentList = new ArrayList<>();
+    private List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {super(manager);}
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return mFragmentTitleList.get(position);
+    }
+
+}
