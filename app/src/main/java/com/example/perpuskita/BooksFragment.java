5f09a8a6cae2d57a4a4cd54d957b60e411767b9d
Yasya Rusyda <yasyars@Yasyas-MacBook-Pro.local>
2019-02-25 00:02:45 +0700

    Finisih book search ui

--- app/src/main/java/com/example/perpuskita/BooksFragment.java
+++ app/src/main/java/com/example/perpuskita/BooksFragment.java
@@ -3,6 +3,7 @@ package com.example.perpuskita;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,8 @@ import android.view.ViewGroup;
  */
 public class BooksFragment extends Fragment {
 
+    SearchView search;
+
 
     public BooksFragment() {
         // Required empty public constructor
@@ -22,8 +25,15 @@ public class BooksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_books, container, false);
+        View v = inflater.inflate(R.layout.fragment_books, container, false);
+
+        search = v.findViewById(R.id.search);
+//        search.setQueryHint("Custom Search Hint");
+
+        return v;
+
     }
 
 }
