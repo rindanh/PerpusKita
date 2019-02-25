80250ade3267ed0cdf7965405fc015343a313013
rinda <rindanurh@gmail.com>
2019-02-25 13:05:16 +0700

    finish login and logout using firebase

--- app/src/main/java/com/example/perpuskita/ProfileFragment.java
+++ app/src/main/java/com/example/perpuskita/ProfileFragment.java
@@ -1,17 +1,30 @@
 package com.example.perpuskita;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    private Button btnLogout;
+    private FirebaseAuth auth;
+    private static final String LOG_TAG = LoginActivity.class.getSimpleName();
 
 
     public ProfileFragment() {
@@ -22,8 +35,30 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v=inflater.inflate(R.layout.fragment_profile, container, false);
+        btnLogout = (Button) v.findViewById(R.id.logout_button);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "LOGOUT BUTTON CLICKED");
+                auth.signOut();
+                startActivity(new Intent(getContext(), LoginActivity.class));
+                getActivity().finish();
+            }
+        });
+        return v;
     }
+//
+//    @Override
+//    public void onClick(View v) {
+//        if (v.getId()==R.id.logout_button)
+//            auth.signOut();
+//    }
+
+
 
 }
--- app/src/main/java/com/example/perpuskita/SplashActivity.java
+++ app/src/main/java/com/example/perpuskita/SplashActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
+import com.google.firebase.FirebaseApp;
+
 public class SplashActivity extends AppCompatActivity {
+    private static final String LOG_TAG = SplashActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_splash);
         ImageView imageView = findViewById(R.id.icon);
         imageView.animate().alpha(0f).setDuration(2500);
