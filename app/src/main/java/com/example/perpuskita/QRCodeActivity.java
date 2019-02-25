a0c43e2723f6b6206e21c5d4fb3d31e8efd28f20
Yasya Rusyda <yasyars@Yasyas-MacBook-Pro.local>
2019-02-25 20:10:59 +0700

    add qr code scanner

--- app/src/main/java/com/example/perpuskita/QRCodeActivity.java
+++ app/src/main/java/com/example/perpuskita/QRCodeActivity.java
@@ -1,13 +1,44 @@
 package com.example.perpuskita;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class QRCodeActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QRCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public  void onPause(){
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result){
+        Log.d("handleResultQR","handleResult: " + result.getText());
+        Intent intent = new Intent();
+        intent.putExtra("token",result.getText());
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
